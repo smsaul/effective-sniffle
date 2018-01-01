95e4afb1bf754364226479af67b75181005ad15a
Seth Saulnier <seth.saulnier@icloud.com>
2018-01-01 17:52:39 -0500

    pooCount v1
    this uses digital up and down to add and subtract poos on loos.
    
    known bugz:
    if you poo more than 10, then less than 10, the zero remains on-screen

---
+++ pooCount/pooCount/pooCount.ino/pooCount.ino.ino
@@ -0,0 +1,37 @@
+#include <LiquidCrystal.h>
+
+int SwitchPinUp = 5;
+int SwitchPinDown = 6;
+int pooCount = 0;
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void setup() {
+  lcd.begin(16, 2);
+  pinMode(SwitchPinUp, INPUT_PULLUP);
+  pinMode(SwitchPinDown, INPUT_PULLUP);
+  Serial.begin(9600);
+  lcd.print("Poos in Loos:");
+}
+
+void loop() {
+  while(pooCount >= 0)
+  {
+  if(digitalRead(SwitchPinUp) == LOW)
+    {
+      pooCount = pooCount + 1;
+      delay(200);
+    }
+  lcd.setCursor(0,1);
+  lcd.print(pooCount);
+  if(digitalRead(SwitchPinDown) == LOW)
+   {
+      if(pooCount >= 1)
+      {
+        pooCount = pooCount - 1;
+        delay(200);
+      }
+   }
+   lcd.setCursor(0,1);
+   lcd.print(pooCount);
+  }
+}
