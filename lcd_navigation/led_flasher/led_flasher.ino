36c087741b0303fddd38711a8fbe806d19e265ea
Seth Saulnier <seth.saulnier@icloud.com>
2018-01-04 11:41:12 -0500

    apparently clearLine isnt a thing

--- lcd_navigation/led_flasher/led_flasher.ino
+++ lcd_navigation/led_flasher/led_flasher.ino
@@ -13,16 +13,19 @@ works appropriately.
 
 for speed and led indicator characters, when the values are reduced, use a
 blank space to clear the character. lcd.print(" ");
+
+consider using variabke marh
+
 */
 
 #include <LiquidCrystal.h>
 
 int index = 0;
-int ledCount = 0;
+int ledCount = 1;
 int ledSpeed = 500;
-int switchPinLeft = 5;
-int switchPinRight = 6;
-int switchPinReset = 3;
+int switchPinLeft = 3;
+int switchPinRight = 5;
+int switchPinReset = 6;
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
@@ -36,8 +39,10 @@ void loop() {
   if(digitalRead(switchPinReset) == LOW)
   {
     index = 0;
-    lcd.clearLine(0);
-    lcd.clearLine(1);
+    lcd.setCursor(0,0);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print("                ");
   }
   while(index == 0)
   {
@@ -50,22 +55,58 @@ void loop() {
     if(digitalRead(switchPinLeft) == LOW)
     {
       index = 1;
-      lcd.clearLine(0);
-      lcd.clearLine(1);
+      lcd.setCursor(0,0);
+      lcd.print("                ");
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+
     }
     if(digitalRead(switchPinRight) == LOW)
     {
       index = 2;
-      lcd.clearLine(0);
-      lcd.clearLine(1);
+      lcd.setCursor(0,0);
+      lcd.print("                ");
+      lcd.setCursor(0,1);
+      lcd.print("                ");
     }
   }
   while(index == 1)
   {
-    /* This is the LED select menu */
+    lcd.setCursor(2,0);
+    lcd.print("LEDs:");
+    lcd.setCursor(2,1);
+    lcd.print("LESS");
+    lcd.setCursor(10,1);
+    lcd.print("MORE");
+    if(digitalRead(switchPinLeft) == LOW)
+    {
+      if(ledCount >= 1 <= 5)
+      {
+        lcd.setCursor((ledCount + 6), 0);
+        lcd.print("X");
+        ledCount = ledCount + 1;
+      }
+    }
+    if(digitalRead(switchPinRight) == LOW)
+    {
+      if(ledCount >= 1 < 5)
+      {
+        lcd.setCursor((ledCount + 6), 1);
+        lcd.print(" ");
+        ledCount = ledCount - 1;
+      }
+    }
   }
+/*
   while(index == 2)
   {
-    /* This is the speed select menu*/
+    lcd.setCursor(1,0);
+    lcd.print("SPEED:");
+    lcd.setCursor(2,1);
+    lcd.print("LESS");
+    lcd.setCursor(10,1);
+    lcd.print("MORE");
+
   }
+  */
 }
